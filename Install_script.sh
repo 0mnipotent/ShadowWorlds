137365191892b17451a23e995c756d5cefe8f9bc
Jackson Danaher <37206426+0mnipotent@users.noreply.github.com>
2022-10-31 13:15:14 +1100

    Update Install_script.sh

--- Install_script.sh
+++ Install_script.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 
-
-
 # Get details for cPanel
 read -p "Enter domain: " cpanel_dom
 
 # Get details for Database
-read -p "Enter server ID [1]: " svr_id
-svr_id=${svr_id:-1}
 read -p "What is the server name: " svr_name
 svr_id=${svr_name:-ShadowWorlds}
 read -p "What is the server IP: " svr_ip
-read -p "What is the server Port [30303] : " svr_port
-svr_port=${svr_port:-30303}
 
 # Set DB Password
 db_password=$(date | md5sum | md5sum | cut -c-12)
-
-# Get details for game config
-read -p "Enter RespawnTimer [30000] :" mud_res
-mud_res=${mud_res:-30000}
-read -p "Enter MonsterTimer [1000] : " mud_mon
-mud_mon=${mud_mon:-1000}
-read -p "Enter RestoreHPAndMNTimer [30000] : " mud_hp
-mud_hp=${mud_hp:-30000}
-read -p "Enter PlayersAttackTimer [1000] : " mud_pla
-mud_pla=${mud_play:-1000}
-read -p "Enter HitsPerDurability [1000] : " mud_dur
-mud_dur=${mud_dur:-1000}
 
 # Install cPanel
 yum remove NetworkManager* -y
@@ -50,8 +32,6 @@ systemctl disable firewalld
 sed -i 's/TESTING = "1"/TESTING = "0"/g' /etc/csf/csf.conf
 sed -i 's/^TCP_IN = "/TCP_IN = "30303,/g' /etc/csf/csf.conf
 sed -i 's/^TCP_OUT = "/TCP_OUT = "30303,/g' /etc/csf/csf.conf
-sed -i 's/^UDP_IN = "/UDP_IN = "30303,/g' /etc/csf/csf.conf
-sed -i 's/^UDP_OUT = "/UDP_OUT = "30303,/g' /etc/csf/csf.conf
 systemctl start csf
 systemctl start lfd
 systemctl enable csf
@@ -71,9 +51,9 @@ mysql sw_db < database.sql
 # Update database server details
 mysql sw_db -e "INSERT INTO servers (id, server_address, pvp, port, num_maps, toplist_path, maxusers, name) VALUES ('$svr_id', '$svr_ip','1','$svr_port','99','','99','$svr_name')"
 
-# Set mudclient.ini config
+# Set sql.ini config
 cd /home/sw/game/server/
-sed -i "s/ID=x/ID=$svr_id/g" mudclient.ini
-sed -i "s/Port=x/ID=$svr_port/g" mudclient.ini
+sed -i "s/mysqlpassword=x/mysqlpassword=$db_password/g" sql.ini
+
 
 
